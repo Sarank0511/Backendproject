a233ecd85f891692bfd2160370677e59a5ea7f1b
Sarank0511 <sarank051120003@gmail.com>
2026-09-10 13:45:52 +0530

    Update WED_HOSTS environment variable

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 WED_HOSTS = os.environ.get(
  "DJANGO_ALLOWED_HOSTS",
- "localhost,127.0.0.1,https://backendproject-lp9i.onrender.com"
+ "localhost,127.0.0.1,backendproject-lp9i.onrender.com"
 ).split(",")
 
 
