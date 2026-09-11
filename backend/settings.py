77f4345faaac2d6cc5a9648fb3714a4e25b368e9
Sarank0511 <sarank051120003@gmail.com>
2026-09-11 12:19:07 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -31,11 +31,6 @@ ALLOWED_HOSTS = [
     "backendproject-lp9i.onrender.com",
 ]
 
-# WED_HOSTS = os.environ.get(
-#  "backendproject-lp9i.onrender.com",
-#  "DJANGO_ALLOWED_HOSTS",
-#  "localhost,127.0.0.1,"
-# ).split(",")
 
 
 # Application definition
